d029914e56972ce882f2c8eb5b2f5fa410ea4436
ibizugbe <abrahamibizugbe@gmail.com>
2024-04-21 16:50:51 +0100

    Resolve git conflict.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import ProductListing from './ProductListing/ProductListing'
 import HomePage from './pages/HomePage'
 function App() {
 
   return (
     <>
       <HomePage/>
-      <Navbar />
-      <Login />
-      <Registration />  
     </>
   )
 }
