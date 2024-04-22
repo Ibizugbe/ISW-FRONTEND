d209275cb563347b61a7ee267b28e247b459f329
ibizugbe <abrahamibizugbe@gmail.com>
2024-04-22 20:12:28 +0100

    Implement error handling to fix bug.

--- src/auth/Registration.jsx
+++ src/auth/Registration.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { register, selectLoading, selectError } from '../redux/slices/authSlice';
 
@@ -6,14 +7,30 @@ const Register = () => {
   const dispatch = useDispatch();
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
-
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleRegister = () => {
-    dispatch(register({ name, email, password }));
-  };
+const handleRegister = () => {
+  dispatch(register({ name, email, password }))
+    .then((response) => {
+      console.log("Registration response:", response); 
+      if (response && response.payload && response.payload._id) {
+        const token = localStorage.getItem('token');
+        navigate("/login");
+      } else {
+        console.error("User information not found in response");
+      }
+    })
+    .catch((error) => {
+      console.error("Registration failed:", error);
+    });
+};
+
+
+
+
 
   return (
     <div>
