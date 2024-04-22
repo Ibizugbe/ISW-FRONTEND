a223527801579dabcd2f8e915b0c5841fd9d0a47
saraphina1 <oyeyemibunmi21@gmail.com>
2024-04-22 22:33:36 +0100

    login and registration page

--- src/auth/Registration.jsx
+++ src/auth/Registration.jsx
@@ -1,22 +1,32 @@
 // Registration.js
 
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { registerRequest, registerSuccess, registerFailure } from '../redux/slices/registrationSlice';
-import axios from 'axios';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  registerRequest,
+  registerSuccess,
+  registerFailure,
+} from "../redux/slices/registrationSlice";
+import axios from "axios";
+import "./style.css";
+import eye from "../assets/eye.png";
+import closedEye from "../assets/closedEye.png";
 
 const Registration = () => {
   const dispatch = useDispatch();
-  const isRegistering = useSelector((state) => state.registration.isRegistering);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const isRegistering = useSelector(
+    (state) => state.registration.isRegistering
+  );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleRegister = async () => {
     dispatch(registerRequest());
 
     try {
       // Make a POST request to the registration endpoint
-      const response = await axios.post('http://127.0.0.1:3000/users/sign_up', {
+      const response = await axios.post("http://127.0.0.1:3000/users/sign_up", {
         username,
         password,
       });
@@ -29,23 +39,97 @@ const Registration = () => {
     }
   };
 
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleRegister} disabled={isRegistering}>
-        {isRegistering ? 'Registering...' : 'Register'}
+    <div className="registration">
+      <div className="headerWrap">
+        <h2 className="Sign">Sign Up</h2>
+        <p>Enter your details to create your account</p>
+        <div className="navButton">
+          <button className="storeOwner">Store owner</button>
+          <button className="shopper">Shopper</button>
+        </div>
+      </div>
+      <div className="signupForm">
+        <label htmlFor="email" className="Label2">
+          Email
+        </label>
+        <br />
+        <input
+          type="email"
+          placeholder=""
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+
+      <div className="signupForm">
+        <label htmlFor="number" className="Label2">
+          Phone Number
+        </label>
+        <br />
+        
+          <input
+            type="number"
+            placeholder="+234 812-345-6789"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        
+        <div className="passwordWrapper">
+          <div className="Password">
+            <label htmlFor="number" className="Label2">
+              Create a password
+            </label>
+            <br />
+            <div className="passwordInput2">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="|"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <img
+                src={showPassword ? closedEye : eye}
+                alt="toggle password visibility"
+                onClick={handleTogglePassword}
+              />
+            </div>
+            <p className="validation">Must have at least 8 characters</p>
+          </div>
+
+          <div className="Password">
+            <label htmlFor="number" className="Label2">
+              Confirm Password
+            </label>
+            <br />
+            <div className="passwordInput2">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="|"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <img
+                src={showPassword ? closedEye : eye}
+                alt="toggle password visibility"
+                onClick={handleTogglePassword}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        className="loginButton"
+        onClick={handleRegister}
+        disabled={isRegistering}
+      >
+        {isRegistering ? "Registering..." : "Create my account"}
       </button>
+
+      <p className="loginAccount">Already have an account? Log In</p>
     </div>
   );
 };
