346ecf8e1002799cfb95ab25da7ae8817f085f79
ibizugbe <abrahamibizugbe@gmail.com>
2024-04-22 09:41:36 +0100

    Update registration component.

--- src/auth/Registration.jsx
+++ src/auth/Registration.jsx
@@ -1,53 +1,30 @@
-// Registration.js
-
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { registerRequest, registerSuccess, registerFailure } from '../redux/slices/registrationSlice';
-import axios from 'axios';
+import { register, selectLoading, selectError } from './slices/authSlice';
 
-const Registration = () => {
+const Register = () => {
   const dispatch = useDispatch();
-  const isRegistering = useSelector((state) => state.registration.isRegistering);
-  const [username, setUsername] = useState('');
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleRegister = async () => {
-    dispatch(registerRequest());
-
-    try {
-      // Make a POST request to the registration endpoint
-      const response = await axios.post('http://127.0.0.1:3000/users/sign_up', {
-        username,
-        password,
-      });
-
-      // On success, dispatch registerSuccess action
-      dispatch(registerSuccess());
-    } catch (error) {
-      // On failure, dispatch registerFailure action with error message
-      dispatch(registerFailure(error.message));
-    }
+  const handleRegister = () => {
+    dispatch(register({ name, email, password }));
   };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleRegister} disabled={isRegistering}>
-        {isRegistering ? 'Registering...' : 'Register'}
-      </button>
+      <h2>Register</h2>
+      {error && <p>{error}</p>}
+      <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleRegister} disabled={loading}>{loading ? 'Registering...' : 'Register'}</button>
     </div>
   );
 };
 
-export default Registration;
+export default Register;
