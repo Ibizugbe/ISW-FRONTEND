aa2e78b8f18b69e51ce9f29d5e9debcabd55b997
Anna Isumonah <maisumonah@gmail.com>
2024-04-22 13:01:14 +0100

    made changes to the navbar and sidebar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,15 +11,15 @@ const HomePage = () => {
 
   return (
     <main className="h-[100vh]">
-      <div className="">
+      <div className="relative">
         <NavBar cartState={cartState} setCartState={setCartState}></NavBar>
       </div>
 
-      <div className={`lg:w-[95%] mx-auto mt-10 lg:mt-32 flex justify-between flex-wrap gap-y-[2rem] ${cartState ? "hidden" : "block"}`}>
-        <div className="lg:w-[25%] w-full fixed z-50 bg-[#F3F3F3] lg:bg-transparent py-6 lg:py-0 border-t border-[#D5D5D5] lg:border-0">
+      <div className={`lg:w-[95%] mx-auto mt-16 lg:mt-28 flex justify-between flex-wrap gap-y-[2rem] ${cartState ? "hidden md:block" : "block"}`}>
+        <div className="lg:w-[25%] w-full fixed top-[20px] lg:top-[120px] z-20 bg-[#F3F3F3] lg:bg-transparent py-6 lg:py-0 border-t border-[#D5D5D5] lg:border-0">
           <SideBar />
         </div>
-        <div className=" w-[90%] mx-auto lg:w-[75%] lg:ml-[20rem] mt-[10rem] sm:mt-[12rem] lg:mt-0">
+        <div className=" w-[90%] mx-auto lg:w-[75%] lg:ml-[20rem] mt-[12rem] sm:mt-[12rem] lg:mt-0">
           <ProductListing />
         </div>
       </div>
