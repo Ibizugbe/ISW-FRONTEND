b803f5bdcb70c831eb02bd1c4b3afb3249ace815
ibizugbe <abrahamibizugbe@gmail.com>
2024-04-23 16:03:01 +0100

    Add cetrgory slice and update store.

---
+++ src/redux/slices/categorySlice.js
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchCategories = createAsyncThunk(
+  'categories/fetchCategories',
+  async () => {
+    const response = await axios.get('/api/categories');
+    return response.data;
+  }
+);
+
+export const addCategory = createAsyncThunk(
+  'categories/addCategory',
+  async (categoryData) => {
+    const response = await axios.post('/api/categories', categoryData);
+    return response.data;
+  }
+);
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: {
+    categories: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.categories = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(addCategory.fulfilled, (state, action) => {
+        state.categories.push(action.payload);
+      });
+  },
+});
+
+export default categoriesSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import productReducer from './slices/productSlice';
+import categoriesReducer from './slices/categoriesSlice';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
+    categories: categoriesReducer,
   },
 });
